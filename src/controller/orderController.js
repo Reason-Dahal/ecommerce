2cab448a6f472696c9a31946dd1f57143de6568b
Reason Dahal <rsondahal@gmail.com>
2026-06-18 18:14:47 +0545

    deleteOrder created

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -56,3 +56,17 @@ export const getAllOrder = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const deleteOrder = async (req, res, next) => {
+    try {
+      const order = await Order.findById(req.params.id);
+      if (!order) {
+        return res.status(404).json({ message: "Order not found" });
+      }
+  
+      await order.deleteOne();
+      res.json({ message: "Order deleted", deletedOrder: order });
+    } catch (error) {
+      next(error);
+    }
+  };
--- src/route/orderRoute.js
+++ src/route/orderRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createOrder, getAllOrder, getMyOrder, updateOrderStatus } from "../controller/orderController.js";
+import { createOrder, getAllOrder, getMyOrder, updateOrderStatus, deleteOrder } from "../controller/orderController.js";
 import { protect,admin } from "../middleware/authMiddleware.js";
 
   const orderRouter = express.Router();
@@ -8,5 +8,6 @@ import { protect,admin } from "../middleware/authMiddleware.js";
  orderRouter.get("/getMyOrder",protect,getMyOrder);
  orderRouter.get("/getAllOrder",protect,admin,getAllOrder);
  orderRouter.put("/updateOrderStatus/:id",protect,admin,updateOrderStatus);
+ orderRouter.delete("/deleteOrder/:id", protect, admin, deleteOrder);
 
  export default orderRouter;
