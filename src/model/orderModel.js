4338d94b678250d6427fcb84a8788d50fad0d3ac
Reason Dahal <rsondahal@gmail.com>
2026-06-18 22:21:32 +0545

    case sensitive error in enum orderModel.js fixed

--- src/model/orderModel.js
+++ src/model/orderModel.js
@@ -54,12 +54,11 @@ const orderSchema = new mongoose.Schema({
 
     },
 
-    status:{
-        type:String,
-        enum:["Pending","Processing","shipped","Delivered"],
-        default:"Pending"
-    }
-
+    status: {
+        type: String,
+        enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
+        default: "pending"
+      }
 
 
 })
